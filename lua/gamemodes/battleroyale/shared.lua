231c7adf630c34bd8083d60fd7f2d358ba4579f2
John McBroom <jimbomcb@gmail.com>
2015-08-07 23:54:56 +0100

    increase mutator chance

--- lua/gamemodes/battleroyale/shared.lua
+++ lua/gamemodes/battleroyale/shared.lua
@@ -11,7 +11,7 @@ GM.Cvars = {}
 GM.Cvars.ForceMutator = CreateConVar( "sv_br_forcemutator", "", FCVAR_NOTIFY + FCVAR_REPLICATED )
 GM.Cvars.ForceWeapon = CreateConVar( "sv_br_forceweapon", "", FCVAR_NOTIFY + FCVAR_REPLICATED )
 GM.Cvars.EnableMutators = CreateConVar( "sv_br_mutators_enabled", "1", FCVAR_NOTIFY + FCVAR_REPLICATED )
-GM.Cvars.MutatorBias = CreateConVar( "sv_br_mutator_bias", "0.35", FCVAR_NOTIFY + FCVAR_REPLICATED )
+GM.Cvars.MutatorBias = CreateConVar( "sv_br_mutator_bias", "0.75", FCVAR_NOTIFY + FCVAR_REPLICATED )
 GM.Cvars.RoundTimeMin = CreateConVar( "sv_br_roundtime_min", "60", FCVAR_NOTIFY + FCVAR_REPLICATED )
 GM.Cvars.RoundTimeMax = CreateConVar( "sv_br_roundtime_max", "110", FCVAR_NOTIFY + FCVAR_REPLICATED )
 
