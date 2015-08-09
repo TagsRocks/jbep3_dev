544eae3f05fd9bc485cd141689c9e461c0dc4137
John McBroom <jimbomcb@gmail.com>
2015-08-10 00:35:12 +0100

    Moonwalking shouldn't allow strafing.

--- lua/gamemodes/battleroyale/mutators/moonwalk.lua
+++ lua/gamemodes/battleroyale/mutators/moonwalk.lua
@@ -6,17 +6,23 @@ mutator.Description = "#JB_BR_RoundTypeMoonwalk_Desc"
 
 function mutator:PlayerSpawned( pl )
 	pl:DisableButtons( IN_FORWARD )
+	pl:DisableButtons( IN_LEFT )
+	pl:DisableButtons( IN_RIGHT )
 	Msg("Disabling in_forward on " .. pl:GetPlayerName() .. "\n")
 end
 
 function mutator:PlayerKilled( pl, info )
 	pl:EnableButtons( IN_FORWARD )
+	pl:EnableButtons( IN_LEFT )
+	pl:EnableButtons( IN_RIGHT )
 end
 
 function mutator:PlayerWon( pl )
 	-- reenable buttons for all players
 	for k, v in pairs( player.GetAll() ) do
 		v:EnableButtons( IN_FORWARD )
+		v:EnableButtons( IN_LEFT )
+		v:EnableButtons( IN_RIGHT )
 	end
 end
 
